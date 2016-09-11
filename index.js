6c54a2556a448e3b7c10b4accc76c270ce6fabbd
satyashani <satyashani@gmail.com>
2016-09-11 15:25:30 +0530

    0.0.4 added index file

---
+++ index.js
@@ -0,0 +1,9 @@
+/* * ************************************************************ 
+ * Date: 11 Sep, 2016
+ * programmer: Shani Mahadeva <satyashani@gmail.com>
+ * Javascript file index.js
+ * *************************************************************** */
+
+var server = require("./lib/server");
+
+module.exports = server;
