590596b01f6e421d4bfc190c2834795989740f2f
satyashani <satyashani@gmail.com>
2016-09-11 15:28:40 +0530

    0.0.5 removed index added main file in package.json

--- index.js
+++
@@ -1,9 +0,0 @@
-/* * ************************************************************ 
- * Date: 11 Sep, 2016
- * programmer: Shani Mahadeva <satyashani@gmail.com>
- * Javascript file index.js
- * *************************************************************** */
-
-var server = require("./lib/server");
-
-module.exports = server;
